29730058f25c5c815e1c9fb77e29c6202c2dc639
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-07-15 22:03:33 +0200

    Add todoArr.js file

---
+++ src/todoArr.js
@@ -0,0 +1,24 @@
+const list = document.getElementById('list')
+
+const todoList = [
+ {
+  description: 'Install and set up webpack',
+  completed: false,
+  index:4,
+ },
+ {
+  description: 'Edit GitHub profile Page',
+  completed: true,
+  index:3,
+ },
+ {
+  description: 'Edit README.md File',
+  completed: false,
+  index:2,
+ },
+ {
+  description: 'Submit the To do project',
+  completed: false,
+  index:1,
+ }
+]
