845821c9cb448d89b019129080b82e2598671428
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-07-16 00:28:13 +0200

    Add style loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+import  "./style.css";
+
+const list = document.getElementById('list')
+
+const todoList = [
+ {
+  description: 'Install and set up webpack',
+  completed: false,
+  index:4,
+ },
+ {
+  description: 'Edit GitHub profile Page',
+  completed: true,
+  index:3,
+ },
+ {
+  description: 'Edit README.md File',
+  completed: false,
+  index:2,
+ },
+ {
+  description: 'Submit the To do project',
+  completed: false,
+  index:1,
+ }
+]
+
 function renderList(arr) {
  list.innerHTML = arr.sort((a, b) => +a.index - +b.index).map((item) => `<li class="flex-row todo" data-id="${item.index}">
  <input type="checkbox" class="checkbox" id="${item.index}" ${item.completed ? 'checked' : ''}>
