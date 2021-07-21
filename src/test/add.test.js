22900da1ce16e2c1122fc387a3abc671567585b7
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-07-21 18:46:15 +0200

    Add add function with tests

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -1,43 +1,82 @@
+/**
+ * @jest-environment jsdom
+ */
 
+const LocalStorage = (function () {
+  let store = {};
 
-const LocalStorage = (function() {
-    let store = {};
-  
-    return {
-      getItem: function(key) {
-        return store[key] || null;
-      },
-      setItem: function(key, value) {
-        store[key] = value.toString();
-      },
-      removeItem: function(key) {
-        delete store[key];
-      },
-      clear: function() {
-        store = {};
-      }
-    };
-  })();
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+}());
+
+function addTodo(event) {
+  const list = JSON.parse(LocalStorage.getItem('todo'));
+  list.push(event);
+  list.forEach((object) => {
+    const li = document.createElement('li');
+    li.classList.add("list")
+    li.innerHTML = `<input type="checkbox" class="checkbox" data-id="${object.index}"  ${object.completed ? 'checked' : ''}>
+        <input type="text" value="${object.description}" data-index="${object.index}" draggable="false" class="todo-text ${object.completed ? 'completed' : ''}">
+        <i class="fas fa-ellipsis-v dots" data-id="${object.index}"></i>
+        <i class="fas fa-trash deleteBtn" data-type="deleteBtn" data-trash="${object.index}"></i>`
+    document.body.appendChild(li);
+  })
+  LocalStorage.setItem('todo', JSON.stringify(list))
+}
+
+function removeTodo(event) {
+  if (event.target.dataset.type === 'deleteBtn') {
+    const todos = getFromStorage('TodoList');
+    todos.splice(event.target.parentNode.id, 1);
+    const modifiedIndex = todos.map((el, index) => ({ ...el, index }));
+    saveToStorage('TodoList', modifiedIndex);
+    renderList(modifiedIndex);
+  }
+}
 
 beforeAll(() => {
-    const list = [
-        { description: 'hiii', completed: false, index: 1},
-        { description: 'hello', completed: false, index: 2}
-    ]
-    LocalStorage.setItem("todo", JSON.stringify(list))
+  const list = [
+    { description: 'hiii', completed: false, index: 1 },
+    { description: 'hello', completed: false, index: 2 },
+  ];
+  LocalStorage.setItem('todo', JSON.stringify(list));
 });
 
 describe('add function', () => {
-    test('list length is 2', () => {
-        const listResult = JSON.parse(LocalStorage.getItem('todo'));
-        expect(listResult.length).toBe(2)
-    })
-    test('add object', () => {
-        const listResult = JSON.parse(LocalStorage.getItem('todo'));
-        const newObj = { description: 'zero', completed: false, index: 3}
-        listResult.push(newObj);
-        LocalStorage.setItem("todo", JSON.stringify(listResult))
-        expect(listResult.length).toBe(3)
-    })
+  test('list length is 2', () => {
+    const listResult = JSON.parse(LocalStorage.getItem('todo'));
+    expect(listResult.length).toBe(2);
+  });
+  test('add object', () => {
+    const listResult = JSON.parse(LocalStorage.getItem('todo'));
+    const newObj = { description: 'zero', completed: false, index: 3 };
+    listResult.push(newObj);
+    LocalStorage.setItem('todo', JSON.stringify(listResult));
+    expect(listResult.length).toBe(3);
+  });
+  test('Add html', () => {
+    const newObj = { description: 'zero', completed: false, index: 3 };
+    addTodo(newObj)
+    const list = document.querySelectorAll('.list');
+    expect(list).toHaveLength(4);
+  })
+});
+
+describe('remove function', () => {
+  test("remove a function from the list", () => {
+    const list = LocalStorage.getItem("todo")
     
+  })
 })
