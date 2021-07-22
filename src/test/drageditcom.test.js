7a813ca380978c5c713e6c36865c7731faf7566f
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-07-22 16:31:19 +0200

    Add test for drag and drop function

--- src/test/drageditcom.test.js
+++ src/test/drageditcom.test.js
@@ -43,6 +43,23 @@ function updateStatus(list, index) {
   return list;
 }
 
+function dragDrop(list, position1, position2) {
+  list.forEach((object) => {
+    const li = document.createElement('li');
+    li.classList.add('list');
+    li.innerHTML = `<input type="checkbox" class="checkbox" data-id="${object.index}"  ${object.completed ? 'checked' : ''}>
+            <input type="text" value="${object.description}" data-index="${object.index}" draggable="false" class="todo-text ${object.completed ? 'completed' : ''}">
+            <i class="fas fa-ellipsis-v dots" data-id="${object.index}"></i>
+            <i class="fas fa-trash deleteBtn" data-type="deleteBtn" data-trash="${object.index}"></i>`;
+    document.body.appendChild(li);
+  });
+  const b = list[position2].index;
+  list[position2].index = list[position1].index;
+  list[position1].index = b;
+  LocalStorage.setItem('todo', JSON.stringify(list));
+  return list;
+}
+
 beforeAll(() => {
   const list = [
     { description: 'hiii', completed: false, index: 1 },
@@ -85,5 +102,18 @@ describe('The function will update completed status', () => {
     const listResult = JSON.parse(LocalStorage.getItem('todo'));
     const checked = updateStatus(listResult, 1);
     expect(checked[1].completed).toBe(false);
+  });
+});
+
+describe('The function will mock drag and drop', () => {
+  test('The position1 and position2 will change index on drag and drop', () => {
+    const listResult = JSON.parse(LocalStorage.getItem('todo'));
+    const swapPlaces = dragDrop(listResult, 0, 1);
+    expect(swapPlaces[0].index).toBe(2);
+  });
+  test('The position1 and position2 will change index on drag and drop again', () => {
+    const listResult = JSON.parse(LocalStorage.getItem('todo'));
+    const swapPlaces = dragDrop(listResult, 0, 1);
+    expect(swapPlaces[0].index).toBe(1);
   });
 });
